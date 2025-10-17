a7bd4d1aa6f9c715adf9387399d7852d84212640
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-17 14:42:41 +0200

    add tasks

--- snack2/script.js
+++ snack2/script.js
@@ -23,3 +23,6 @@ function getRandomNumberr(min, max) {
 
 /* Creazione array con dentro le squadre di calcio come oggetti */ 
 
+/* Generazione di numeri random per i falli subiti ed i punti fatti */
+
+/* Creazione array con dentro solo nomi e falli subiti delle squadre */
