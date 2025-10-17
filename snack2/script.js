280eea1864aa0040764c7795b4b87fa6d577c33f
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-17 14:40:31 +0200

    aff funtion for random

--- snack2/script.js
+++ snack2/script.js
@@ -7,3 +7,19 @@ Tools:
 - for
 - function random
 */
+
+// ========================================================================
+// ========================================================================
+
+/* Inizializzazione funzioni */
+
+// Funzione random
+function getRandomNumberr(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// =========================================================================
+// =========================================================================
+
+/* Creazione array con dentro le squadre di calcio come oggetti */ 
+
