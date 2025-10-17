0893e5ea158dde1a23c267061c31d9709a156679
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-17 13:46:06 +0200

    add tasks

--- snack1/script.js
+++ snack1/script.js
@@ -7,3 +7,12 @@ Tools:
 - for
 - if / else
 */
+
+// ==========================================================
+// ==========================================================
+
+/* Creazione array con oggetti al suo interno */
+
+/* Confrontare il peso delle bici per trovare la meno pesante */
+
+/* Mostrare a schermo la bici meno pesante */
