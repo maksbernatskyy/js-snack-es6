c2d5bb779372c3a98db37870e1aaf71fe76d75cc
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-17 13:52:05 +0200

    add array with objects

--- snack1/script.js
+++ snack1/script.js
@@ -13,6 +13,30 @@ Tools:
 
 /* Creazione array con oggetti al suo interno */
 
+// Creo l'array
+const bikes = [
+    {
+        nome: 'Trek Marlin',
+        peso: 10
+    },
+    {
+        nome: 'Atala Clever',
+        peso: 8
+    },
+    {
+        nome: 'Trek Allant+',
+        peso: 9
+    },
+    {
+        nome: 'Bianchi Lupo',
+        peso: 12
+    },
+    {
+        nome: 'Scott Addict',
+        peso: 20
+    },
+]
+
 /* Confrontare il peso delle bici per trovare la meno pesante */
 
 /* Mostrare a schermo la bici meno pesante */
